1d08a2292c190268c8f0873a2bede146fa8259c7
Nate Mueller <inathan.m@gmail.com>
2024-07-04 09:29:27 -0500

    typos

--- src/app/services/treasureHunt.ts
+++ src/app/services/treasureHunt.ts
@@ -8,7 +8,7 @@ import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client"
 export const TREASURE_HUNT_ADDRESS = "0xDDEFa1ea12Fa171586028a0a6e328c0cB57B46B2"
 export const LENS_HUB_ADDRESS = "0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d"
 
-const madfiSubgraphUrl = `https://gateway-arbitrum.network.thegraph.com/api/${process.env.NEXT_PUBLIC_MONEY_CLUBS_SUBGRAPH_API_KEY}/subgraphs/id/BT7yTf18FbLQpbZ35k9sTnQ8PVNEjG3QgbsggCMnC6oU`
+const madfiSubgraphUrl = `https://gateway-arbitrum.network.thegraph.com/api/${process.env.MONEY_CLUBS_SUBGRAPH_API_KEY}/subgraphs/id/BT7yTf18FbLQpbZ35k9sTnQ8PVNEjG3QgbsggCMnC6oU`
 
 const TREASURE_HUNT = gql`
   query Hunt($id: Bytes!) {
@@ -20,6 +20,17 @@ const TREASURE_HUNT = gql`
   }
 `
 
+export const convertIntToHexLensId = (profileId: string) => {
+  let hexProfileId = parseInt(profileId).toString(16)
+  // If the hex parsed profile id is an odd number length then it needs to be padded with a zero after the 0x
+  if (hexProfileId.length % 2 !== 0) {
+    hexProfileId = "0x0" + hexProfileId
+  } else {
+    hexProfileId = "0x" + hexProfileId
+  }
+  return hexProfileId
+}
+
 const subgraphClient = () => {
   return new ApolloClient({
     ssrMode: typeof window === "undefined", // set to true for server-side rendering
@@ -29,18 +40,24 @@ const subgraphClient = () => {
 }
 
 export const isTreasureFound = async (address, huntId) => {
+  const huntIdFormatted = convertIntToHexLensId(huntId.toString())
+  console.log(huntIdFormatted)
   const { data } = await subgraphClient().query({
     query: TREASURE_HUNT,
-    variables: { id: huntId },
+    variables: { id: huntIdFormatted },
   })
 
+  console.log(data)
+
   const treasureClue = data.treasureHunt.treasure
-  return await publicClient.readContract({
-    address: TREASURE_HUNT_ADDRESS,
-    abi: TreasureHuntAbi,
-    functionName: "foundClue",
-    args: [huntId, address, treasureClue],
-  })
+  if (treasureClue !== null) {
+    return await publicClient.readContract({
+      address: TREASURE_HUNT_ADDRESS,
+      abi: TreasureHuntAbi,
+      functionName: "foundClue",
+      args: [huntId, address, treasureClue],
+    })
+  } else return false
 }
 
 export const postToHunt = async (profileId, pubId) => {
