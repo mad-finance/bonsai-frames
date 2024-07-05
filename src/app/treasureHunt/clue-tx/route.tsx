72a67b3adf84958774ac195ecb2d978ff3a74a07
Nate Mueller <inathan.m@gmail.com>
2024-07-05 11:05:40 -0500

    logs

--- src/app/treasureHunt/clue-tx/route.tsx
+++ src/app/treasureHunt/clue-tx/route.tsx
@@ -6,10 +6,22 @@ import { LENS_HUB_ADDRESS, TREASURE_HUNT_ADDRESS } from "@/app/services/treasure
 
 export const POST = frames(async (ctx) => {
   if (!ctx.message) throw new Error("No message")
+  if (!ctx.state) throw new Error("No state")
 
   console.log("message", ctx.message)
+  console.log("state", ctx.state)
 
   const pubParts = ctx.message?.pubId.split("-")
+
+  console.log("args", {
+    publicationActedProfileId: pubParts[0],
+    publicationActedId: pubParts[1],
+    actorProfileId: ctx.message?.profileId,
+    referrerProfileIds: [],
+    referrerPubIds: [],
+    actionModuleAddress: TREASURE_HUNT_ADDRESS,
+    actionModuleData: ctx.state?.secret,
+  })
 
   const calldata = encodeFunctionData({
     abi: LensHubAbi,
