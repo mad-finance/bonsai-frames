78e1ed17cf86e7b6e003224b46da1abe73c28b9f
Nate Mueller <inathan.m@gmail.com>
2024-07-31 15:20:36 -0700

    fonts

--- src/app/cashtags/images/route.tsx
+++ src/app/cashtags/images/route.tsx
@@ -1,3 +1,4 @@
+/*
 import { createImagesWorker } from "frames.js/middleware/images-worker/next";
 import * as fs from "node:fs"
 import * as path from "node:path"
@@ -29,6 +30,34 @@ const imagesRoute = createImagesWorker({
       },
     ],
   },
+});
+
+export const GET = imagesRoute();
+*/
+
+
+import { createImagesWorker } from "frames.js/middleware/images-worker/next";
+// import path from "path";
+// import { readFileSync } from "fs";
+
+// Add this line to the route that will be handling images
+export const runtime = "edge";
+
+// const topSecret = readFileSync(
+//   path.join(path.resolve(process.cwd(), "public"), "TopSecret.ttf")
+// );
+
+const imagesRoute = createImagesWorker({
+  secret: process.env.IMAGE_WORKER_SECRET,
+  // imageOptions: {
+  //   fonts: [
+  //     {
+  //       name: "TopSecret",
+  //       data: topSecret,
+  //       weight: 400,
+  //     },
+  //   ],
+  // },
 });
 
 export const GET = imagesRoute();
