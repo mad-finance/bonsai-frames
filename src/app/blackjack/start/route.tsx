966c2a3ec6653b2659ebee220d4cde56e1fd111b
Nate Mueller <inathan.m@gmail.com>
2024-07-31 14:58:43 -0700

    everything

--- src/app/blackjack/start/route.tsx
+++ src/app/blackjack/start/route.tsx
@@ -1,22 +1,23 @@
 import { Button } from "frames.js/next"
 import { frames } from "../frames"
 
-
-
-
 const handleRequest = frames(async () => {
   return {
     image: (
-      <div tw="flex w-full h-full items-center justify-center" style={{ 
-        backgroundImage: `url(http://localhost:3000/blackjack/blackjack-start-bg.jpg)`,
-        backgroundSize: 'cover'
+      <div
+        tw="flex w-full h-full items-center justify-center"
+        style={{
+          backgroundImage: `url(http://localhost:3000/blackjack/blackjack-start-bg.jpg)`,
+          backgroundSize: "cover",
         }}
-        >
-      </div>
+      ></div>
     ),
     buttons: [
+      <Button action="post" key="button" target="/learn">
+        How to Play
+      </Button>,
       <Button action="post" key="button" target="/table">
-        Start Playing ♠	♥	♦	♣
+        Start Playing ♠ ♥ ♦ ♣
       </Button>,
     ],
   }
