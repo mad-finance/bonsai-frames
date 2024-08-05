d5421f69c45efcaa9d9109c7c8840abda9f5c098
Nate Mueller <inathan.m@gmail.com>
2024-08-05 12:25:05 -0700

    move stuff around

--- src/app/blackjack/approve-status/route.tsx
+++ src/app/blackjack/approve-status/route.tsx
@@ -8,58 +8,63 @@ export const POST = frames(async (ctx) => {
   const currentState = ctx.state
   const transactionId = ctx.message?.actionResponse || currentState.transactionId
 
-  console.log(ctx.state, ctx.message)
-
-  const transaction = await publicClient.getTransactionReceipt({
-    hash: transactionId! as `0x${string}`,
-  })
-
   const buttons: any[] = []
-  const txPending = !transaction?.blockHash
-  if (transactionId && txPending) {
-    buttons.push(
-      <Button action="link" target={polygonScanUrl(transactionId! as `0x${string}`)}>
-        View on block explorer
-      </Button>
-    )
+  let txPending = false
+
+  if (transactionId) {
+    const transaction = await publicClient.getTransactionReceipt({
+      hash: transactionId! as `0x${string}`,
+    })
+
+    txPending = !transaction?.blockHash
+    if (transactionId && txPending) {
+      buttons.push(
+        <Button action="link" target={polygonScanUrl(transactionId! as `0x${string}`)}>
+          View on block explorer
+        </Button>
+      )
+    }
+
+    if (txPending) {
+      buttons.push(
+        <Button action="post" target="/play-status">
+          Refresh Status
+        </Button>
+      )
+    }
   }
 
-  if (txPending) {
-    buttons.push(
-      <Button action="post" target="/approve-status">
-        Refresh Status
-      </Button>
-    )
-  }
   buttons.push(
     <Button action="post" target="/table">
       Return to Table
     </Button>
   )
 
-  const updatedState = { ...currentState, transactionId }
-
   return {
     image: (
       <div
         tw="flex w-full h-full relative items-center justify-center"
         style={{
-          backgroundImage: `url(${baseUrl}/blackjack/${txPending ? "blackjack-table-pending-static.jpg" : "blackjack-table-transaction-complete.jpg"})`,
+          backgroundImage: `url(${baseUrl}/blackjack/${
+            txPending
+              ? "blackjack-table-pending-static.jpg"
+              : "blackjack-table-transaction-complete.jpg"
+          })`,
           backgroundSize: "cover",
           fontFamily: "'Verdana', monospace",
           fontWeight: 700,
           color: "#FFFFFF",
         }}
       >
-        { /* 
+        {/* 
         <div tw="flex flex-col items-center">
           <p>&nbsp;</p>
           <p tw="m-10">{txPending ? "Transaction Pending..." : "Transaction Complete!"}</p>
         </div>
-      */ }
+      */}
       </div>
     ),
     buttons,
-    state: updatedState,
+    state: { ...currentState, transactionId },
   }
 })
--- src/app/blackjack/play-status/route.tsx
+++ src/app/blackjack/play-status/route.tsx
@@ -8,29 +8,32 @@ export const POST = frames(async (ctx) => {
   const currentState = ctx.state
   const transactionId = ctx.message?.actionResponse || currentState.transactionId
 
-  console.log(ctx.state, ctx.message)
-
-  const transaction = await publicClient.getTransactionReceipt({
-    hash: transactionId! as `0x${string}`,
-  })
-
   const buttons: any[] = []
-  const txPending = !transaction?.blockHash
-  if (transactionId && txPending) {
-    buttons.push(
-      <Button action="link" target={polygonScanUrl(transactionId! as `0x${string}`)}>
-        View on block explorer
-      </Button>
-    )
+  let txPending = false
+
+  if (transactionId) {
+    const transaction = await publicClient.getTransactionReceipt({
+      hash: transactionId! as `0x${string}`,
+    })
+
+    txPending = !transaction?.blockHash
+    if (transactionId && txPending) {
+      buttons.push(
+        <Button action="link" target={polygonScanUrl(transactionId! as `0x${string}`)}>
+          View on block explorer
+        </Button>
+      )
+    }
+
+    if (txPending) {
+      buttons.push(
+        <Button action="post" target="/play-status">
+          Refresh Status
+        </Button>
+      )
+    }
   }
 
-  if (txPending) {
-    buttons.push(
-      <Button action="post" target="/play-status">
-        Refresh Status
-      </Button>
-    )
-  }
   buttons.push(
     <Button action="post" target="/table">
       Return to Table
@@ -42,21 +45,25 @@ export const POST = frames(async (ctx) => {
       <div
         tw="flex w-full h-full relative items-center justify-center"
         style={{
-          backgroundImage: `url(${baseUrl}/blackjack/` + (txPending ? "blackjack-table-pending.gif" : "blackjack-table-transaction-complete.jpg") + `)`,
+          backgroundImage:
+            `url(${baseUrl}/blackjack/` +
+            (txPending
+              ? "blackjack-table-pending.gif"
+              : "blackjack-table-transaction-complete.jpg") +
+            `)`,
           backgroundSize: "cover",
           fontFamily: "'Verdana', monospace",
           fontWeight: 700,
           color: "#FFFFFF",
         }}
       >
-      { /* 
+        {/* 
         <div tw="flex flex-col items-center">
           <p>&nbsp;</p>
           <p tw="m-10">{txPending ? "Transaction Pending..." : "Transaction Complete!"}</p>
         </div>
-      */ }
-      
-        </div>
+      */}
+      </div>
     ),
     buttons,
     state: { ...currentState, transactionId },
