7bfdc59040c6a8ada58c0e78440df779b4bb6ba9
Nate Mueller <inathan.m@gmail.com>
2024-08-12 00:13:44 -0500

    play without lens

--- src/app/blackjack/close-tx/route.tsx
+++ src/app/blackjack/close-tx/route.tsx
@@ -1,45 +1,66 @@
 import LensHubAbi from "../../services/abi/LensHub.json"
+import BlackjackAbi from "../../services/abi/Blackjack.json"
 import { frames } from "../frames"
 import { transaction } from "frames.js/core"
 import { Abi, encodeFunctionData } from "viem"
-import { BLACKJACK_ADDRESS, getModuleData } from "@/app/services/blackjack"
+import { BLACKJACK_ADDRESS, getModuleData, getTableId } from "@/app/services/blackjack"
 import { LENS_HUB_ADDRESS } from "@/app/services/treasureHunt"
 
 export const POST = frames(async (ctx) => {
   if (!ctx.message) throw new Error("No message")
 
-  const pubParts = ctx.message?.pubId.split("-")
-  // placeholder empty rng - it doesn't get checked
-  const signedRNG = {
-    seedOne: BigInt(0),
-    seedTwo: BigInt(0),
-    signature: `0x`,
-  }
-  const moduleData = getModuleData("CLOSE", signedRNG)
+  if (ctx?.message?.url?.includes("?table=")) {
+    // call contract directly
+    const calldata = encodeFunctionData({
+      abi: BlackjackAbi,
+      functionName: "closeGame",
+      args: [getTableId(ctx), ctx.state.owner],
+    })
 
-  const calldata = encodeFunctionData({
-    abi: LensHubAbi,
-    functionName: "act",
-    args: [
-      {
-        publicationActedProfileId: pubParts[0],
-        publicationActedId: pubParts[1],
-        actorProfileId: ctx.message?.profileId,
-        referrerProfileIds: [],
-        referrerPubIds: [],
-        actionModuleAddress: BLACKJACK_ADDRESS,
-        actionModuleData: moduleData,
+    return transaction({
+      chainId: "eip155:137",
+      method: "eth_sendTransaction",
+      params: {
+        abi: BlackjackAbi as Abi,
+        to: BLACKJACK_ADDRESS,
+        data: calldata,
       },
-    ],
-  })
+    })
+  } else {
+    // act on lens post
+    const pubParts = ctx.message?.pubId.split("-")
 
-  return transaction({
-    chainId: "eip155:137",
-    method: "eth_sendTransaction",
-    params: {
-      abi: LensHubAbi as Abi,
-      to: LENS_HUB_ADDRESS,
-      data: calldata,
-    },
-  })
+    // placeholder empty rng - it doesn't get checked
+    const signedRNG = {
+      seedOne: BigInt(0),
+      seedTwo: BigInt(0),
+      signature: `0x`,
+    }
+    const moduleData = getModuleData("CLOSE", signedRNG)
+    const calldata = encodeFunctionData({
+      abi: LensHubAbi,
+      functionName: "act",
+      args: [
+        {
+          publicationActedProfileId: pubParts[0],
+          publicationActedId: pubParts[1],
+          actorProfileId: ctx.message?.profileId,
+          referrerProfileIds: [],
+          referrerPubIds: [],
+          actionModuleAddress: BLACKJACK_ADDRESS,
+          actionModuleData: moduleData,
+        },
+      ],
+    })
+
+    return transaction({
+      chainId: "eip155:137",
+      method: "eth_sendTransaction",
+      params: {
+        abi: LensHubAbi as Abi,
+        to: LENS_HUB_ADDRESS,
+        data: calldata,
+      },
+    })
+  }
 })
--- src/app/blackjack/stand-tx/route.tsx
+++ src/app/blackjack/stand-tx/route.tsx
@@ -1,8 +1,14 @@
 import LensHubAbi from "../../services/abi/LensHub.json"
+import BlackjackAbi from "../../services/abi/Blackjack.json"
 import { frames } from "../frames"
 import { transaction } from "frames.js/core"
 import { Abi, encodeFunctionData } from "viem"
-import { BLACKJACK_ADDRESS, getModuleData, getSignedRNG } from "@/app/services/blackjack"
+import {
+  BLACKJACK_ADDRESS,
+  getModuleData,
+  getSignedRNG,
+  getTableId,
+} from "@/app/services/blackjack"
 import { LENS_HUB_ADDRESS } from "@/app/services/treasureHunt"
 
 export const POST = frames(async (ctx) => {
@@ -10,31 +16,57 @@ export const POST = frames(async (ctx) => {
 
   const pubParts = ctx.message?.pubId.split("-")
   const signedRNG = await getSignedRNG()
-  const moduleData = getModuleData("STAND", signedRNG)
 
-  const calldata = encodeFunctionData({
-    abi: LensHubAbi,
-    functionName: "act",
-    args: [
-      {
-        publicationActedProfileId: pubParts[0],
-        publicationActedId: pubParts[1],
-        actorProfileId: ctx.message?.profileId,
-        referrerProfileIds: [],
-        referrerPubIds: [],
-        actionModuleAddress: BLACKJACK_ADDRESS,
-        actionModuleData: moduleData,
+  if (ctx?.message?.url?.includes("?table=")) {
+    // call contract directly
+    const calldata = encodeFunctionData({
+      abi: BlackjackAbi,
+      functionName: "play",
+      args: [
+        1,
+        getTableId(ctx),
+        signedRNG.signature,
+        { seedOne: signedRNG.seedOne, seedTwo: signedRNG.seedTwo },
+      ],
+    })
+
+    return transaction({
+      chainId: "eip155:137",
+      method: "eth_sendTransaction",
+      params: {
+        abi: BlackjackAbi as Abi,
+        to: BLACKJACK_ADDRESS,
+        data: calldata,
       },
-    ],
-  })
+    })
+  } else {
+    // act on lens post
+    const moduleData = getModuleData("STAND", signedRNG)
 
-  return transaction({
-    chainId: "eip155:137",
-    method: "eth_sendTransaction",
-    params: {
-      abi: LensHubAbi as Abi,
-      to: LENS_HUB_ADDRESS,
-      data: calldata,
-    },
-  })
+    const calldata = encodeFunctionData({
+      abi: LensHubAbi,
+      functionName: "act",
+      args: [
+        {
+          publicationActedProfileId: pubParts[0],
+          publicationActedId: pubParts[1],
+          actorProfileId: ctx.message?.profileId,
+          referrerProfileIds: [],
+          referrerPubIds: [],
+          actionModuleAddress: BLACKJACK_ADDRESS,
+          actionModuleData: moduleData,
+        },
+      ],
+    })
+
+    return transaction({
+      chainId: "eip155:137",
+      method: "eth_sendTransaction",
+      params: {
+        abi: LensHubAbi as Abi,
+        to: LENS_HUB_ADDRESS,
+        data: calldata,
+      },
+    })
+  }
 })
